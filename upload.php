55c54678f8b5bf8f31a6f43df376e6094b8311c3
conniealta <conniealta@gmail.com>
2018-11-23 09:03:59 +0100

    ok

--- upload.php
+++ upload.php
@@ -22,11 +22,6 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 5) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
