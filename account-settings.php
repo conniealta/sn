5df123435bf9d2187787524fb38a8aa798210f5d
conniealta <conniealta@gmail.com>
2018-12-04 08:44:30 +0100

    ok

--- account-settings.php
+++ account-settings.php
@@ -89,92 +89,91 @@ if ($userid == $followerid) { //nur wenn die eingeloggte Person  auf ihrer eigen
 
 
 
+<br><br><br><br>
 
-<!-- Das ganze Formular: -->
+    <form action="do-account-settings.php" method="post">
 
-<!--    <form action="do-account-settings.php" method="post">-->
-<!---->
-<!---->
-<!--        <p>Username:-->
-<!--            <input type="text" size="40" maxlength="250" name="username" placeholder="Username eingeben"></p>-->
-<!---->
-<!--        <p>Email:-->
-<!--            <input type="text" size="40" maxlength="250" name="email" placeholder="E-Mail eingeben"></p>-->
-<!---->
-<!--        <p>Passwort:-->
-<!--            <input type="password" size="40"  maxlength="250" name="passwort" placeholder="••••••"></p>-->
-<!---->
-<!--        <br><br>-->
-<!---->
-<!--        <p>Vorname:-->
-<!--        <input type="text" size="40" maxlength="250" name="fname" placeholder="Vorname eingeben"></p>-->
-<!---->
-<!--        <p>Nachname:-->
-<!--        <input type="text" size="40" maxlength="250" name="lname" placeholder="Nachname eingeben"></p>-->
-<!---->
-<!---->
-<!--        <p>Alter:-->
-<!--            <input type="text" size="40" maxlength="10" name="age" placeholder="Alter eingeben"></p>-->
-<!---->
-<!--        <p>Heimatstadt/-land:-->
-<!--            <input type="text" size="40" maxlength="1000" name="heimat" placeholder="Heimatstadt/-land eingeben"></p>-->
-<!---->
-<!--        <p>Sprachkenntnisse:-->
-<!--            <input type="text" size="40" maxlength="1000" name="sprachen" placeholder="Sprachen eingeben"></p>-->
-<!---->
-<!--        <p>Studiengang:-->
-<!--        <select name="studiengang">-->
-<!--            <option value="omm">Online-Medien-Management</option>-->
-<!--            <option value="wi">Wirtschaftsinformatik und digitale Medien</option>-->
-<!--            <option value="id">Informationsdesign</option>-->
-<!--            <option value="bm">Bibliotheksmanagement</option>-->
-<!--            <option value="am">Audiovisuelle Medien</option>-->
-<!--            <option value="cr">Crossmedia Redaktion/Public Relations</option>-->
-<!--            <option value="mt">Deutsch-chinseischer Studiengang Medien und Technologie</option>-->
-<!--            <option value="iw">Informationswissenschaften</option>-->
-<!--            <option value="ip">Integriertes Produktdesign</option>-->
-<!--            <option value="mp">Mediapublishing</option>-->
-<!--            <option value="mi">Medieninformatik</option>-->
-<!--            <option value="mw">Medienwirtschaft</option>-->
-<!--            <option value="mm">Mobile Medien</option>-->
-<!--            <option value="pmt">Print Media Technologies</option>-->
-<!--            <option value="vt">Verpackungstechnik</option>-->
-<!--            <option value="wm">Werbung und Marktkommunikation</option>-->
-<!--            <option value="wim">Wirtschaftsingenieurwesen Medien</option>-->
-<!--        </select>-->
-<!--        </p>-->
-<!---->
-<!--        <p>Aktuelles Semester:-->
-<!--            <select name="semester">-->
-<!--                <option value="1">1. Semester</option>-->
-<!--                <option value="2">2. Semester</option>-->
-<!--                <option value="3">3. Semester</option>-->
-<!--                <option value="4">4. Semester</option>-->
-<!--                <option value="5">5. Semester</option>-->
-<!--                <option value="6">6. Semester</option>-->
-<!--                <option value="7">7. Semester</option>-->
-<!--            </select>-->
-<!---->
-<!--        <p>Job:-->
-<!--            <input type="text" size="40" maxlength="1000" name="job" placeholder="Job eingeben"></p>-->
-<!---->
-<!--        <p>Interessen:-->
-<!--        <input type="text" size="40" maxlength="1000" name="interessen" placeholder="Interessen eingeben"></p>-->
-<!---->
-<!--        <p>Lieblingszitat:-->
-<!--            <input type="text" size="40" maxlength="1000" name="zitat" placeholder="Zitat eingeben"></p>-->
-<!---->
-<!--        <p>Website:-->
-<!--            <input type="text" size="40" maxlength="1000" name="website" placeholder="Website eingeben"></p>-->
-<!---->
-<!--        <p>Handynummer:-->
-<!--            <input type="text" size="40" maxlength="1000" name="handy" placeholder="Handynummer eingeben"></p>-->
-<!---->
-<!--        <br><br>-->
-<!--        <input type="submit" name="update" value="Ändere deine Angaben">-->
-<!---->
-<!---->
-<!--    </form>-->
+
+        <p>Username:
+            <input type="text" size="40" maxlength="250" name="username" placeholder="Username eingeben"></p>
+
+        <p>Email:
+            <input type="text" size="40" maxlength="250" name="email" placeholder="E-Mail eingeben"></p>
+
+        <p>Passwort:
+            <input type="password" size="40"  maxlength="250" name="passwort" placeholder="••••••"></p>
+
+        <br><br>
+
+        <p>Vorname:
+        <input type="text" size="40" maxlength="250" name="fname" placeholder="Vorname eingeben"></p>
+
+        <p>Nachname:
+        <input type="text" size="40" maxlength="250" name="lname" placeholder="Nachname eingeben"></p>
+
+
+        <p>Alter:
+            <input type="text" size="40" maxlength="10" name="age" placeholder="Alter eingeben"></p>
+
+        <p>Heimatstadt/-land:
+            <input type="text" size="40" maxlength="1000" name="heimat" placeholder="Heimatstadt/-land eingeben"></p>
+
+        <p>Sprachkenntnisse:
+            <input type="text" size="40" maxlength="1000" name="sprachen" placeholder="Sprachen eingeben"></p>
+
+        <p>Studiengang:
+        <select name="studiengang">
+            <option value="omm">Online-Medien-Management</option>
+            <option value="wi">Wirtschaftsinformatik und digitale Medien</option>
+            <option value="id">Informationsdesign</option>
+            <option value="bm">Bibliotheksmanagement</option>
+            <option value="am">Audiovisuelle Medien</option>
+            <option value="cr">Crossmedia Redaktion/Public Relations</option>
+            <option value="mt">Deutsch-chinseischer Studiengang Medien und Technologie</option>
+            <option value="iw">Informationswissenschaften</option>
+            <option value="ip">Integriertes Produktdesign</option>
+            <option value="mp">Mediapublishing</option>
+            <option value="mi">Medieninformatik</option>
+            <option value="mw">Medienwirtschaft</option>
+            <option value="mm">Mobile Medien</option>
+            <option value="pmt">Print Media Technologies</option>
+            <option value="vt">Verpackungstechnik</option>
+            <option value="wm">Werbung und Marktkommunikation</option>
+            <option value="wim">Wirtschaftsingenieurwesen Medien</option>
+        </select>
+        </p>
+
+        <p>Aktuelles Semester:
+            <select name="semester">
+                <option value="1">1. Semester</option>
+                <option value="2">2. Semester</option>
+                <option value="3">3. Semester</option>
+                <option value="4">4. Semester</option>
+                <option value="5">5. Semester</option>
+                <option value="6">6. Semester</option>
+                <option value="7">7. Semester</option>
+            </select>
+
+        <p>Job:
+            <input type="text" size="40" maxlength="1000" name="job" placeholder="Job eingeben"></p>
+
+        <p>Interessen:
+        <input type="text" size="40" maxlength="1000" name="interessen" placeholder="Interessen eingeben"></p>
+
+        <p>Lieblingszitat:
+            <input type="text" size="40" maxlength="1000" name="zitat" placeholder="Zitat eingeben"></p>
+
+        <p>Website:
+            <input type="text" size="40" maxlength="1000" name="website" placeholder="Website eingeben"></p>
+
+        <p>Handynummer:
+            <input type="text" size="40" maxlength="1000" name="handy" placeholder="Handynummer eingeben"></p>
+
+        <br><br>
+        <input type="submit" name="update" value="Ändere deine Angaben">
+
+
+    </form>
 
 
 </body>
