481ccbcf9d8bdc9d9e2442d8bcaf3bbb99991ec4
conniealta <conniealta@gmail.com>
2018-12-11 17:41:07 +0100

    ok

--- user_data.php
+++ user_data.php
@@ -18,7 +18,7 @@ else {
 include('DB.php');
 include ('db_pdo.php');
 
-// Fetch von allen Variablen in unserer User-Datenbanktabelle:
+// Fetch von allen Variablen in unserer User-Datenbanktabelle (eingeloggte Person):
 
 //$userid = DB::query('SELECT id FROM list5 WHERE id=:userid', array(':userid'=>$user_loggedin))[0]['id'];
 $lname = DB::query('SELECT last_name FROM list5 WHERE id=:userid', array(':userid' => $user_loggedin))[0]['last_name'];
@@ -44,6 +44,7 @@ $followerid = $user_loggedin;
 // $profile_pic2 = das ist das Profilbild der Person, auf deren Profilseite wir sind
 // userid = das ist die id der Person, auf deren Profilseite wir sind
 
+// Fetch der Variablen / Nutzerinfos der Person, auf deren Profilseite wir sind:
 $username = DB::query('SELECT username FROM list5 WHERE username=:username', array(':username'=>$_GET['username']))[0]['username'];
 $userid = DB::query('SELECT id FROM list5 WHERE username=:username', array(':username'=>$_GET['username']))[0]['id'];
 $lname2 = DB::query('SELECT last_name FROM list5 WHERE id=:userid', array(':userid' => $userid))[0]['last_name'];
