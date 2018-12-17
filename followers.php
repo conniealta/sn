ae3215f35e8052b8d809d37d7db91e80b14208b0
conniealta <conniealta@gmail.com>
2018-12-17 12:41:52 +0100

    ok

--- followers.php
+++ followers.php
@@ -1,24 +1,4 @@
 <?php
 session_start();
 include('header.php'); // in "header.php" ist auch "user_data.php" inkludiert und $_SESSION["angemeldet"];
-
-
-
-$userid = DB::query('SELECT id FROM list5 WHERE username=:username', array(':username'=>$_GET['username']))[0]['id'];
-// "$userid" ist die "id" der Person, auf deren Profilseite wir sind (das kann auch unsere Profilseite sein, aber auch die Profilseite von einem anderen Benutzer)
-
-/* beim "$userid" wird die "id" gespeichert, die dem "username" in der Datenbank gehört, der wiederum dem ":username" in der URL entsprechen muss
-  -> anders formuliert: die "id" aus der Datenbank auswählen, wo "username" in der Datenbank dem ":username" in der URL entspricht
-   und diese "id" dann bei der Variable "$userid" speichern */
-
-
-
-
-$follower = DB::query('SELECT follower_id FROM followers, list5 WHERE list5.id=:user_id', array(':user_id'=>$userid));
-
-
-    print $follower;
-
-
-
 ?>
--- following.php
+++ following.php
@@ -1,5 +1,25 @@
 <?php
 session_start();
 include('header.php'); // in "header.php" ist auch "user_data.php" inkludiert und $_SESSION["angemeldet"];
-
 ?>
+
+<br><br><br>
+<h1> Following - Wem folge ich? </h1>
+<br><br>
+
+<?php
+$dbfollowers = DB::query('SELECT list5.username, list5.profile_pic FROM list5, followers
+                             WHERE list5.id = followers.user_id
+                             AND follower_id = :userid', array(':userid'=>$user_loggedin));
+
+
+foreach ($dbfollowers as $f) {
+
+    //echo $f['username'];
+    $username = $f['username'];
+
+    echo "<img style='width: 75px; height: 75px; border-radius: 55px; margin-left:10px;' src='img_upload/profile_pics/" . $f['profile_pic'] . "'>" . ' ' . ' ' . "<a href='profile.php?username=" . $username . " ' >" . $f['username']. '</a>' ;
+echo "<hr>";
+}
+?>
+
