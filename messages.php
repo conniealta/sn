b77cf03dc8c0668acd7c723d7fb743837baa3f66
Bettina <ba033@hdm-stuttgart.de>
2019-01-25 08:54:41 +0100

    hi

--- messages.php
+++ messages.php
@@ -31,7 +31,7 @@ else {
 
 
 
-<h1>Send a Message</h1>
+<h1>Sende eine Nachricht</h1>
 <form action="messages.php?receiver=<?php echo htmlspecialchars($_GET['receiver']); ?>" method="post">
     <textarea name="body" rows="8" cols="80"></textarea>
     <input type="submit" name="send" value="Send Message">
