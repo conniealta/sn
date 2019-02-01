535116b9d80b4efa29cef23bb099c72fb7cb5cdc
conniealta <conniealta@gmail.com>
2019-02-01 13:03:34 +0100

    ok

--- Comment.php
+++ Comment.php
@@ -27,10 +27,6 @@ class Comment {
 
             echo "<div class='comment_box'><img style='width: 35px; height: 35px; border-radius: 55px;' src='img_upload/profile_pics/$profile_pic'>".' '.' '."<a href='profile.php?username=".$username." ' >".$comment['username'].'</a>'.' '.' '.' '.Post::link_add($comment['comment'])."</div>";
 
-           /* if ($userid = $loggedIn_userid){
-               echo "<input type='submit' name='deletecomment' value='Löschen'> ";
-            }*/
-
         }
         // ['comment'] = die Spalte in der Datenbank
     }
@@ -50,36 +46,5 @@ class Comment {
         // ['comment'] = die Spalte in der Datenbank
         return $com;
     }
-
-
-
-//Bei "$com" --> das hinzufügen:
-//$posts .= "<img src='img_upload/profile_pics/".$profilePic."'>.<img src='img_upload/post_pics/".$p['img_id']."'>".self::link_add($p['body']) . "
-//              <form action='profile.php?username=$username&postid=" . $p['id'] . "' method='post'>
-//                 <input type='submit' name='unlike' value='Unlike'>
-//                 <span>".$p['likes']." likes</span>
-//
-//
-//                ";
-//
-//if ($userid == $loggedIn_userid){
-//$posts .="<input type='submit' name='deletepost' value='Löschen'> ";
-//}
-//#damit die Löschen Buttons nur sichtbar auf dem eigenen Profil sind
-//#$userid == $loggedIn_userid
-//
-//$posts .= "<form action='profile.php?postid=".$p['id']." 'method='post'>
-//              <textarea name='commentbody' rows='3' cols='50'></textarea>
-//              <input type='submit' name='comment' value='Kommentieren'>
-//              </form>";
-//
-//
-//// $posts .= Comment::displayComments($p['id']);
-//
-//
-//
-//$posts .="
-//              </form><hr /></br />
-//                ";
 }
 ?>
